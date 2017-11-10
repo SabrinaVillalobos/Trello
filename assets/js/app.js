a2dea7e6d42215da2dec82e29525d532b361cc83
Wafflo <wafflish@gmail.com>
2017-11-10 00:15:54 -0300

    trying to make this work!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,4 @@
-  /*var addlist = document.getElementById("addlist");
-  var form =document.getElementById("form");
-
-window.addEventListener("load", func);
-
-  
-
-   /* addlist.addEventListener("click", pageload);
-
-    var addList = document.getElementById("container");
-    if (addList.style.display === "none"){
-      addList.style.display = "block";
-    }else{
-      addList.style.display="none";
-    }
-  }*/
-
-
- document.getElementById("firstform").style.visibility='hidden'; 
+document.getElementById("firstform").style.visibility='hidden'; 
 
 document.getElementById("container").onclick= function() {disappear()};
 document.getElementById("container").onclick= function() {appearAddList()};
@@ -29,6 +11,38 @@ function appearAddList(){
   document.getElementById("firstform").style.visibility='visible';
   document.getElementById("container").style.visibility="hidden";
 }
+
+document.getElementById("addlist").addEventListener("click", hideAddList);
+function hideAddList () {
+  document.getElementById("firstform").style.visibility="hidden";
+}
+
+
+
+document.getElementById("savebtn").addEventListener("click", newDiv);
+function newDiv () {
+      var contenedorLista = document.createElement("div");
+      contenedorLista.classList.add("newd");
+      var currentDiv = document.getElementById("newl"); 
+  document.body.insertBefore(contenedorLista,currentDiv); 
+    }
+
+
+  document.getElementById("savebtn").addEventListener("click", stopDefAction);
+    function stopDefAction(evt) {
+      evt.preventDefault();
+        document.getElementById("firstform").style.visibility="hidden";
+
+}
+
+
+/*
+document.getElementById("savebtn").addEventListener("submit", disFF);
+function disFF (){
+  document.getElementById("firstform").style.visibility="hidden";
+}*/
+
+
 
 
 
