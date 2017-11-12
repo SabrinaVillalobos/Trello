b45ced2d63b08ee4e1f1ecf5470353ed466d3292
Wafflo <wafflish@gmail.com>
2017-11-12 02:29:15 -0300

    added comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,26 @@
-document.getElementById("firstform").style.visibility = 'hidden';
+document.getElementById("firstform").style.visibility = 'hidden'; //escondo el primer contenedor
 
 document.getElementById("container").onclick = function() {
-    disappear()
+    disappear()//llama la función que esconde el primer contenedor
 };
 document.getElementById("container").onclick = function() {
-    appearAddList()
+    appearAddList()//llama la función que muestra el primer contenedor
 };
 
 
-function disappear() {
+function disappear() {//función que esconde el primer contenedor
     document.getElementById("container").style.visibility = 'hidden';
 }
 
-function appearAddList() {
+function appearAddList() {//función que muestra el formulario "Añadir una lista"
     document.getElementById("firstform").style.visibility = 'visible';
-    document.getElementById("container").style.visibility = "hidden";
+    document.getElementById("container").style.visibility = "hidden";//esconde el primer contenedor
 }
 
 document.getElementById("addlist").addEventListener("click", hideAddList);
 
 function hideAddList() {
-    document.getElementById("firstform").style.visibility = "hidden";
+    document.getElementById("firstform").style.visibility = "hidden";//esconde el formulario
 }
 
 
@@ -28,12 +28,12 @@ function hideAddList() {
 document.getElementById("savebtn").addEventListener("click", newDiv);
 
 function newDiv() {
-    var contenedorLista = document.createElement("div");
-    contenedorLista.classList.add("newd");
-    contenedorLista.setAttribute("id", "newdid");
+    var contenedorLista = document.createElement("div");//nuevo div contenedor
+    contenedorLista.classList.add("newd");//agregando clase a contenedor
+    contenedorLista.setAttribute("id", "newdid");//añadiendo atributo a contenedor
     var currentDiv = document.getElementById("newl");
     document.body.insertBefore(contenedorLista, currentDiv);
-    var listname = document.getElementById("realaddlist").value;
+    var listname = document.getElementById("realaddlist").value;//tomando valor del textarea
 
 }
 
@@ -41,7 +41,7 @@ function newDiv() {
 document.getElementById("savebtn").addEventListener("click", stopDefAction);
 
 function stopDefAction(evt) {
-    evt.preventDefault();
+    evt.preventDefault();//que no se resetee la pagina al enviar el input
 
 }
 
@@ -49,7 +49,7 @@ function stopDefAction(evt) {
 document.getElementById("savebtn").addEventListener("click", disFF);
 
 function disFF() {
-    document.getElementById("firstform").style.display = "none";
+    document.getElementById("firstform").style.display = "none";//desaparece el primer textarea
 
 }
 document.getElementById("savebtn").addEventListener("click", namel);
@@ -64,7 +64,7 @@ function namel() {
 
 document.getElementById("savebtn").addEventListener("click", linen);
 
-function linen() {
+function linen() { //función que guarda el texto del input y lo muestra en una tarjeta
 
     var cdiv = document.getElementById("newl");
 
@@ -86,7 +86,7 @@ function linen() {
 
 }
 
-function tarea() {
+function tarea() {//crea un nuevo contenedor, esconde el anterior
 
     var contenedorLista2 = document.createElement("div");
     contenedorLista2.classList.add("newd2");
@@ -124,7 +124,7 @@ function focF() {
     document.getElementById("focid").style.backgroundColor = "#BFBFBF"
 }
 
-function newCard() {
+function newCard() {//función que crea un nuevo contenedor con txt area, botón y esconde el contenedor anterior
     var newTxt = document.createElement("div");
     newTxt.setAttribute("id", "newTxt");
     var newTxtCont = document.getElementById("newl");
@@ -153,11 +153,12 @@ function newCard() {
 
 
     document.getElementById("btnAdd2").addEventListener("click", ListA);
+      document.getElementById("focid2").addEventListener("focus", focF2);
 
 
 }
 
-function ListA (){
+function ListA (){ //función que crea otro contenedor con txt area y botón
  var newTxt2 = document.createElement("div");
     newTxt2.setAttribute("id", "newTxt2");
     var newTxtCont2 = document.getElementById("newl");
@@ -165,6 +166,9 @@ function ListA (){
     newTxt2.innerHTML = document.getElementById("focid2").value;
         document.getElementById("contN").appendChild(newTxt2);
         document.getElementById("focid2").value = "";
-         document.body.insertBefore(newTxt2, focid2);
+        
 }
 
+function focF2() {
+    document.getElementById("focid2").style.backgroundColor = "#BFBFBF"
+}
